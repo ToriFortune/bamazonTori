3f16f943b6a82e2c504d40a0eaf0088929b927c3
Tori Fortune <toriolley@gmail.com>
2019-05-11 09:54:19 -0500

    updated .env file, included password in customer and manager files

--- bamazonTeeCustomer.js
+++ bamazonTeeCustomer.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: process.env.SQL_PASSWORD,
     database: "bamazonTee"
 });
 
--- bamazonTeeManager.js
+++ bamazonTeeManager.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: process.env.SQL_PASSWORD,
     database: "bamazonTee"
 });
 
